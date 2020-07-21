fe40c9e8722e8c2b759b82222e1acc4b7e496127
bernardopiane <bernardopiane@users.noreply.github.com>
2020-07-21 13:23:32 -0300

    Updates

---
+++ precache-manifest.cd7906bd2465b60de8b4303f0c6d1dfe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641431a498d06263b65c13eb4410cd8e",
+    "url": "/mint/index.html"
+  },
+  {
+    "revision": "8290d9a67f3859dd6d59",
+    "url": "/mint/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "67242e5c68a430eca22d",
+    "url": "/mint/static/css/main.3917be5c.chunk.css"
+  },
+  {
+    "revision": "8290d9a67f3859dd6d59",
+    "url": "/mint/static/js/2.8356cb64.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mint/static/js/2.8356cb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67242e5c68a430eca22d",
+    "url": "/mint/static/js/main.e55f4aab.chunk.js"
+  },
+  {
+    "revision": "89738c5338a90ddf044a",
+    "url": "/mint/static/js/runtime-main.7eb39175.js"
+  },
+  {
+    "revision": "7fe4cbb95af02782b3c047de59e7b82c",
+    "url": "/mint/static/media/face1.7fe4cbb9.jpg"
+  },
+  {
+    "revision": "a1504e3e50678180605d511065ba0856",
+    "url": "/mint/static/media/face2.a1504e3e.jpg"
+  }
+]);
